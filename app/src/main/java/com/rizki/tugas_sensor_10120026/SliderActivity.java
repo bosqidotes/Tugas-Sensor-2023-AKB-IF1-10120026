190e13a66e0e1f05345439144245ee8f95c0c2ce
bosqidotes <rizkiramdani295@gmail.com>
2023-06-22 23:46:00 +0700

    finished project

---
+++ app/src/main/java/com/rizki/tugas_sensor_10120026/SliderActivity.java
@@ -0,0 +1,74 @@
+package com.rizki.tugas_sensor_10120026;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+//Nama  : Rizki Ramdani
+//NIM   : 10120026
+//Kelas : IF 1
+
+public class SliderActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    Button tombolVP;
+    int[] layouts;
+    Adapter_VP adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider);
+
+        viewPager = findViewById(R.id.Pager);
+        tombolVP = findViewById(R.id.tombolViewPager);
+
+        layouts = new int[]{
+                R.layout.vp1_layout,
+                R.layout.vp2_layout,
+                R.layout.vp3_layout
+        };
+
+        adapter = new Adapter_VP(this, layouts);
+        viewPager.setAdapter(adapter);
+        tombolVP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (viewPager.getCurrentItem() + 1 < layouts.length) {
+                    viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                } else {
+                    //kembali ke main activity
+                    startActivity(new Intent(SliderActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        });
+
+        viewPager.addOnPageChangeListener(viewPagerChangeListener);
+    }
+
+    ViewPager.OnPageChangeListener viewPagerChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            if(i == layouts.length -1 ){
+                tombolVP.setText("Mulai");
+            }else {
+                tombolVP.setText("Selanjutnya");
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
---
+++ app/src/main/java/com/rizki/tugas_sensor_10120026/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.rizki.tugas_sensor_10120026;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//Nama  : Rizki Ramdani
+//NIM   : 10120026
+//Kelas : IF 1
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Menghilangkan Action Bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Thread thread = new Thread(){
+            public  void run(){
+                try {
+                    sleep(2000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(SplashScreen.this, SliderActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
